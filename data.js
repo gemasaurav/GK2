14b713adfdedcd47519023c40351fb3bec10602a
gemasaurav <gema.saurav@gmail.com>
2026-07-11 20:37:13 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,93 @@
+const countries = [
 
+{
+name:"India",
+flag:"🇮🇳",
+capital:"New Delhi",
+continent:"Asia",
+animal:"Royal Bengal Tiger",
+bird:"Indian Peacock"
+},
+
+{
+name:"USA",
+flag:"🇺🇸",
+capital:"Washington D.C.",
+continent:"North America",
+animal:"Bald Eagle",
+bird:"Bald Eagle"
+},
+
+{
+name:"United Kingdom",
+flag:"🇬🇧",
+capital:"London",
+continent:"Europe",
+animal:"Lion",
+bird:"European Robin"
+},
+
+{
+name:"France",
+flag:"🇫🇷",
+capital:"Paris",
+continent:"Europe",
+animal:"Gallic Rooster",
+bird:"Gallic Rooster"
+},
+
+{
+name:"Germany",
+flag:"🇩🇪",
+capital:"Berlin",
+continent:"Europe",
+animal:"Federal Eagle",
+bird:"White Stork"
+},
+
+{
+name:"Russia",
+flag:"🇷🇺",
+capital:"Moscow",
+continent:"Europe/Asia",
+animal:"Brown Bear",
+bird:"Double-headed Eagle"
+},
+
+{
+name:"China",
+flag:"🇨🇳",
+capital:"Beijing",
+continent:"Asia",
+animal:"Giant Panda",
+bird:"Red-crowned Crane"
+},
+
+{
+name:"Japan",
+flag:"🇯🇵",
+capital:"Tokyo",
+continent:"Asia",
+animal:"Japanese Macaque",
+bird:"Green Pheasant"
+},
+
+{
+name:"Australia",
+flag:"🇦🇺",
+capital:"Canberra",
+continent:"Australia",
+animal:"Red Kangaroo",
+bird:"Emu"
+},
+
+{
+name:"Canada",
+flag:"🇨🇦",
+capital:"Ottawa",
+continent:"North America",
+animal:"Beaver",
+bird:"Canada Goose"
+}
+
+];
