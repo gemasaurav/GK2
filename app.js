a38e1959b332309163c43f03fdfccf2f60ed9a7d
gemasaurav <gema.saurav@gmail.com>
2026-07-11 18:51:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+setTimeout(function(){
 
+document.getElementById("splashScreen").style.display="none";
+
+document.getElementById("homeScreen").style.display="flex";
+
+},5000);
+
+
+document.getElementById("countryBtn").addEventListener("click",function(){
+
+alert("Countries Section Coming Next");
+
+});
+
+
+document.getElementById("indiaBtn").addEventListener("click",function(){
+
+alert("India Section Coming Next");
+
+});
